06aa40205b85490c00cfb28ceb267dcc5f99892e
Ifeoluwa <ifeoluwa95samuel@gmail.com>
2025-10-18 15:50:23 +0100

    Update scripts.py

--- Python_Codes/scripts.py
+++ Python_Codes/scripts.py
@@ -1,1 +1,11 @@
-
+# Checks if a file already exists
+unique_files = []
+file_list = ['report.csv', 'data.xlsx', 'summary.docx', 'data.csv',]
+for file in file_list:
+    if file not in unique_file:
+        unique_files.append(file)
+    else:
+        print(f'{file} is a duplicate')
+        break
+else:
+    print('All files are unique')
