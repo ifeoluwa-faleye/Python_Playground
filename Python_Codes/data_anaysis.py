2299ceb83b80e787327c69f92d672b46bd7cb621
Ifeoluwa <ifeoluwa95samuel@gmail.com>
2025-11-04 21:03:49 +0000

    Update data_anaysis.py

--- Python_Codes/data_anaysis.py
+++ Python_Codes/data_anaysis.py
@@ -9,3 +9,8 @@ df[['job_title_short','job_title']].head()
 df['salary_year_avg'].describe()
 df.job_title_short.unique()
 df[(df.job_title_short == "Data Analyst") & (df.salary_year_avg.notna())]
+df['job_posted_date'] = pd.to_datetime(df.job_posted_date) #converts job_posted_date from string to datetime
+df['job_posted_month'] = df.job_posted_date.dt.month #add new column job_posted_month
+df.sort_values('job_posted_date', inplace = True) #sort the date by job_posted_date
+df.drop('salary_hour_avg', axis = 1, inplace = True) #drop salary_hour_avg column
+df.dropna(subset = ['salary_year_avg'], inplace = True) #drops null values for salary_year_avg
