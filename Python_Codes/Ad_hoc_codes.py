c045334a4a121b1b139c40f968ac0fdec7d3d8e0
Ifeoluwa <ifeoluwa95samuel@gmail.com>
2026-01-28 17:04:09 +0000

    Update and rename Ad_hoc_codes to Ad_hoc_codes.py

--- Python_Codes/Ad_hoc_codes.py
+++ Python_Codes/Ad_hoc_codes.py
@@ -7,3 +7,12 @@ df = spark.sql("""
     SELECT * FROM workspace.salesdb.dim_customers
     """)
 df.display()
+from pyspark.sql import functions as F
+df = spark.table("workspace.salesdb.dim_customers")
+df.display()
+
+
+df = spark.sql("""
+    SELECT * FROM workspace.salesdb.dim_customers
+    """)
+df.display()
