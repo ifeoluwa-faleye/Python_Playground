b91bb196cce332d554a95f87c8d7c4904c6e81cf
Ifeoluwa <ifeoluwa95samuel@gmail.com>
2026-01-29 17:11:10 +0000

    Update Ad_hoc_codes.py

--- Python_Codes/Ad_hoc_codes.py
+++ Python_Codes/Ad_hoc_codes.py
@@ -6,13 +6,3 @@ df.display()
 df = spark.sql("""
     SELECT * FROM workspace.salesdb.dim_customers
     """)
-df.display()
-from pyspark.sql import functions as F
-df = spark.table("workspace.salesdb.dim_customers")
-df.display()
-
-
-df = spark.sql("""
-    SELECT * FROM workspace.salesdb.dim_customers
-    """)
-df.display()
