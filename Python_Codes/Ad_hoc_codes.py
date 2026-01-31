0f8d3150a0c443f7003c38f49bb9738a4ad27469
Ifeoluwa <ifeoluwa95samuel@gmail.com>
2026-01-31 16:07:56 +0000

    Update Ad_hoc_codes.py

--- Python_Codes/Ad_hoc_codes.py
+++ Python_Codes/Ad_hoc_codes.py
@@ -1,16 +1,7 @@
-from pyspark.sql import functions as F
-df = spark.table("workspace.salesdb.dim_customers")
+# Reading files from CSV
+df = (
+    spark.read.option("header", "true")
+    .option("inferSchema", "true")
+    .csv("/Volumes/workspace/bronze/source_system/source_crm/prd_info.csv")
+)
 df.display()
-
-
-df = spark.sql("""
-    SELECT * FROM workspace.salesdb.dim_customers
-    """)
-from pyspark.sql import functions as F
-df = spark.table("workspace.salesdb.dim_customers")
-df.display()
-
-
-df = spark.sql("""
-    SELECT * FROM workspace.salesdb.dim_customers
-    """)
