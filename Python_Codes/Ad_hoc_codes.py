811f90f96be30180700cea150ed6c0f89cc7bc1e
Ifeoluwa <ifeoluwa95samuel@gmail.com>
2026-02-01 17:41:04 +0000

    Update Ad_hoc_codes.py

--- Python_Codes/Ad_hoc_codes.py
+++ Python_Codes/Ad_hoc_codes.py
@@ -5,5 +5,13 @@ df = (
     .csv("/Volumes/workspace/bronze/source_system/source_crm/prd_info.csv")
 )
 df.display()
+df = (
+    spark.read.option("header", "true")
+    .option("inferSchema", "true")
+    .csv("/Volumes/workspace/bronze/source_system/source_crm/sales_details.csv")
+)
+df.display()
 # Writing to Bronze Layer
 df.write.mode("overwrite").saveAsTable("bronze.crm_prd_info")
+df.write.mode("overwrite").saveAsTable("bronze.crm_sales_details")
+
